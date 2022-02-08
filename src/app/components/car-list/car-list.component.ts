941a148459996c8881dadf375501ec3c36b23231
lashagelashvili <las.gelashvili@gmail.com>
2022-02-08 23:39:49 +0400

    request car list

---
+++ src/app/components/car-list/car-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../services/api.service';
+import { Car } from '../services/model';
+
+@Component({
+  selector: 'app-car-list',
+  templateUrl: './car-list.component.html',
+  styleUrls: ['./car-list.component.css']
+})
+export class CarListComponent implements OnInit {
+
+  cars: Car[] = [];
+
+  constructor(private apiService: ApiService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.refresh();
+  }
+
+  refresh() {
+    this.apiService.getCars().subscribe(data => {
+      console.log(data)
+      this.cars = data;
+    })
+  }
+
+  onCarClick(id: number) {
+    this.router.navigateByUrl(`cars/edit/${id}`);
+  }
+
+  onDelete(id: number) {
+    this.apiService.deleteCar(id).subscribe(data => this.refresh());
+  }
+}
