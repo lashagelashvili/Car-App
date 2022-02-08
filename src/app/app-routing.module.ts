bb37a353968e6b0f60809689e6f775b869c19ad4
lashagelashvili <las.gelashvili@gmail.com>
2022-02-08 23:35:41 +0400

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarEditComponent } from './components/car-edit/car-edit.component';
+import { CarListComponent } from './components/car-list/car-list.component';
+import { CarNewComponent } from './components/car-new/car-new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cars', component: CarListComponent },
+  { path: 'cars/new', component: CarNewComponent },
+  { path: 'cars/edit/:id', component: CarEditComponent },
+  { path: '', redirectTo: '/cars', pathMatch: 'full' },
+  { path: '**', redirectTo: '/cars' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
